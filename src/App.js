1e549a698868ab55a86613e827efa85cb0cb76f2
Alex <alexander.hernandez.contractor@cabify.com>
2021-03-16 01:13:15 +0100

    feat: first stable version

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Cards from './components/Cards'
 /* 
   Debido a que considero que es una "app sencilla", no haremos uso de Router muy complicado
   sino que más bien trataremos el cambio de Vistas mediante un estado general del Componente (componentState)
-  state = 0 -> vista Inicio
-  state = 1 -> vista cartas
-  state = 2 -> vista Resultado 
+  state = 0 -> "vista" Inicio
+  state = 1 -> "vista" cartas
+  state = 2 -> "vista" Resultado (En principio iba a meter una vista más pero lo dejaré como un subEstado del componente Card)
+
+  Mejorar: 
+    -contador tiempo
+    -número de cartas dinámico
+    -resultado acorde con el número de cartas (dinámico)
 */
 
 function App() {
   const INIT_VIEW = 0
   const CARDS_VIEW = 1
-  const RESULT_VIEW = 2
-  const [componentState, setComponentState] = useState(CARDS_VIEW)
+  const [componentState, setComponentState] = useState(INIT_VIEW)
 
   const handleClick = () => {
     return setComponentState(CARDS_VIEW)
@@ -27,16 +31,11 @@ function App() {
         <>
           <img src={logo} alt="logo-app" title="logo-app" />
           <h1 className="app-title">MeMemory</h1>
-          <button className="start-button" onClick={handleClick}>Comenzar</button>
+          <button className="primary-button" onClick={handleClick}>Comenzar</button>
         </>
       }
       {componentState === CARDS_VIEW &&
-        <>
-          <Cards />
-        </>
-      }
-      {componentState === RESULT_VIEW &&
-        <></>
+        <Cards setComponentState={setComponentState} />
       }
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,28 +1,98 @@
-import React, { useState, useEffect } from 'react'
-import arrayCards from '../helpers/CardsData.js'
+import React, { useState, useEffect, useRef } from 'react'
+import arrayCards from '../helpers/cardsData.js'
+import shuffle from '../helpers/shuffleArray.js'
+import { interval, clear } from '../helpers/timer.js'
+import Modal from './Modal'
 
-function Cards() {
+const RESULT_SUCCESS = 9
+
+function Cards({ setComponentState }) {
+    const timerRef = useRef(null);
+    const [cardsSelected, setCardsSelected] = useState([])
+    const [successCounter, setSuccessCounter] = useState(0)
+    const [shuffleArrayCards, setShuffleArrayCards] = useState([])
     //recogemos la lista de cards
     let list = document.getElementsByClassName("card-item")
 
-    const cardSelected = (card) => {
-
+    //seleccionar cartas
+    const selectCard = (card, index) => {
+        const verifyCard = cardsSelected.map(item => item.index === card.index)[0]
+        // si estamos pulsando la misma card -> no hacemos nada (sin especificación)
+        if (verifyCard) {
+            return null
+        }
         for (let item in list) {
-            if (item == card) {
-                let element = list[item]
-                return element.classList.add("selected");
+            if (item == index) {
+                let element = list[index]
+                let imageElement = list[index].getElementsByClassName("card-image")[0]
+                imageElement.classList.add("show-card");
+                element.classList.add("selected");
             }
         }
+        return setCardsSelected(prevState => [...prevState, card])
     }
-    const showCard = () => {
-
+    //evaluar cartas seleccionadas
+    const evalCards = () => {
+        if (cardsSelected[0].pairsId === cardsSelected[1].pairsId) {
+            setSuccessCounter(successCounter + 1)
+        } else {
+            //le damos la vuelta a las cards
+            setTimeout(() => {
+                for (let item in list) {
+                    if (list[cardsSelected[item]?.idHtml]) {
+                        list[cardsSelected[item]?.idHtml].classList.remove("selected");
+                        let imageElement = list[cardsSelected[item]?.idHtml].getElementsByClassName("card-image")[0]
+                        imageElement.classList.remove("show-card");
+                    }
+                }
+            }, 1000)
+        }
+        // vaciamos el array de cartas seleccionadas
+        return setCardsSelected([])
     }
 
-
+    //Main useEffect -> reordenamos el array y seteamos el intervalo
+    useEffect(() => {
+        setShuffleArrayCards(shuffle(arrayCards))
+        //setShuffleArrayCards(arrayCards)
+        if (timerRef.current !== undefined) {
+            interval()
+        }
+    }, [])
+    //controlando el par de cartas seleccionadas
+    useEffect(() => {
+        if (cardsSelected.length === 2) {
+            evalCards()
+        }
+    }, [cardsSelected])
+    //controlando el contador de aciertos
+    //se podría mejorar muchísimo (más dinámico), en cuanto se metan más cartas el juego dejaría de funcionar =/
+    useEffect(() => {
+        if (successCounter === RESULT_SUCCESS) {
+            clear()
+        }
+    }, [successCounter])
     return (
-        <ul className="card-list">
-            {arrayCards?.map((item, index) => <li className={"card-item"} key={index} onClick={() => cardSelected(index)}><span className="span-number">{item.index}</span></li>)}
-        </ul>
+        <>
+            <p>Aciertos: {successCounter}</p>
+            <p>Tiempo: <span id="timer" ref={timerRef}></span></p>
+            <ol className="card-list">
+                {
+                    shuffleArrayCards?.map((item, index) => {
+                        //meehh - Mejorar!!!!
+                        item.idHtml = index
+                        return (
+                            <li className={`card-item index-${index}`} key={index} onClick={() => selectCard(item, index)}>
+                                <img className={`card-image`} src={require(`../assets/images/${item.image}.png`).default} alt={`card-app-${index}`} title={`card-app-${index}`} />
+                            </li>
+                        )
+                    })
+                }
+            </ol>
+            {successCounter === RESULT_SUCCESS &&
+                <Modal setComponentState={setComponentState} time={timerRef.current.innerHTML} />
+            }
+        </>
     );
 }
 
