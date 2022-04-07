257d333dccee57921fffc4e5ef7461519372b8b3
danielsoto <danielsotogarcia@gmail.com>
2022-04-07 12:59:47 +0200

    feat: elimina key innecesario

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,7 +21,7 @@ export default function Card({
 
   return (
     <div className="card">
-      <div className={flipped ? "flipped" : ""} key={card.id}>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card_front" />
         <img
           className="reverse"
