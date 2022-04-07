387bc973711415f8dc2a0897a55912b0c255fb77
danielsoto <danielsotogarcia@gmail.com>
2022-04-07 14:17:43 +0200

    refactor: unifica useEffects y quita silenciador de eslint

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,28 +5,18 @@ export default function Timer({ gameOver, handleTimeRecord }) {
   const [init] = useState(+new Date());
   const [diff, setDiff] = useState(null);
 
-  const tick = () => {
-    setDiff(new Date(+new Date() - init));
-  };
-
   useEffect(() => {
-    if (!gameOver && init) {
+    const tick = () => {
+      setDiff(new Date(+new Date() - init));
+    };
+
+    if (!gameOver && (diff || init)) {
       requestAnimationFrame(tick);
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    if (!gameOver && diff) {
-      requestAnimationFrame(tick);
-    }
-  }, [diff, gameOver]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    if (gameOver) {
+    } else {
       cancelAnimationFrame(tick);
       handleTimeRecord(timeFormat(diff));
     }
-  }, [gameOver]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [init, diff, gameOver, handleTimeRecord]);
 
   return (
     <span data-testid="testid-timer" className="timer">
