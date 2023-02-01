7a07349a5a13dfdf3ee4e6745985b77a37ca9cf2
javierscode <javihp79@gmail.com>
2023-02-01 14:41:50 +0100

    feat: add images & constants

---
+++ src/constants/index.ts
@@ -0,0 +1,42 @@
+import { Item } from '../entities/item.entity'
+
+export const COVER_IMAGE = '/images/cover.svg'
+
+export const AVAILABLE_ITEMS: Item[] = [
+  {
+    key: 'cat',
+    image: '/images/cat.png'
+  },
+  {
+    key: 'crying',
+    image: '/images/crying.png'
+  },
+  {
+    key: 'doge',
+    image: '/images/doge.png'
+  },
+  {
+    key: 'girl',
+    image: '/images/girl.png'
+  },
+  {
+    key: 'patricio',
+    image: '/images/patricio.png'
+  },
+  {
+    key: 'poker-face',
+    image: '/images/poker-face.png'
+  },
+  {
+    key: 'smile',
+    image: '/images/smile.png'
+  },
+  {
+    key: 'suspect',
+    image: '/images/suspect.png'
+  },
+  {
+    key: 'think',
+    image: '/images/think.png'
+  }
+]
