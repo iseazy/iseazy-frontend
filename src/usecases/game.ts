7bdd18cb872830f708f505745a3512339c593ed6
javierscode <javihp79@gmail.com>
2023-02-02 13:01:58 +0100

    refactor: replace board entity for array of items

--- src/usecases/game.ts
+++ src/usecases/game.ts
@@ -1,10 +1,10 @@
-import { Board } from '../entities/board.entity'
 import { AVAILABLE_ITEMS } from '../constants'
 import { Game } from '../entities/game.entity'
+import { Item } from '../entities/item.entity'
 
 export function initializeBoard() {
   const allItemsPairs = [...AVAILABLE_ITEMS, ...AVAILABLE_ITEMS]
-  const board: Board = []
+  const board: Item[] = []
 
   for (let i = 0; i < AVAILABLE_ITEMS.length * 2; i++) {
     const randomIndex = Math.floor(Math.random() * allItemsPairs.length)
@@ -24,12 +24,15 @@ export function initializeGame(): Game {
   }
 }
 
-export function isTheSameItemSelected(board: Board, selectedItems: Game['selectedItems']): boolean {
+export function isTheSameItemSelected(
+  board: Item[],
+  selectedItems: Game['selectedItems']
+): boolean {
   const [firstItem, secondItem] = selectedItems
   return board[firstItem].key === board[secondItem].key
 }
 
-export function isTheGameOver(board: Board, matchedItems: Game['matchedItems']): boolean {
+export function isTheGameOver(board: Item[], matchedItems: Game['matchedItems']): boolean {
   return matchedItems.length === board.length / 2 && matchedItems.length === AVAILABLE_ITEMS.length
 }
 
