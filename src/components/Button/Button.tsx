d79ef6ad869783b368eb81415571965f2c961157
javierscode <javihp79@gmail.com>
2023-02-02 13:07:30 +0100

    feat: add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import styles from './Button.module.css'
+
+interface ButtonProps {
+  children: React.ReactNode
+  onClick: () => void
+}
+
+export function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
