222901247157f5934df216f3be80127496b068be
javierscode <javihp79@gmail.com>
2023-02-02 13:09:29 +0100

    feat: add styles to home view

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,18 @@
+import { Button } from '../../components/Button'
+import styles from './Home.module.css'
+
 interface HomeProps {
   startGame: () => void
 }
 
 export function Home({ startGame }: HomeProps) {
   return (
-    <div className='Home'>
-      <h1>MeMemory</h1>
-      <button onClick={startGame}>Start Game</button>
-    </div>
+    <main className={styles.home}>
+      <figure className={styles.figure}>
+        <img src='/images/logo.svg' alt='MeMemory Game Logo' />
+      </figure>
+      <h1 className={styles.title}>MeMemory</h1>
+      <Button onClick={startGame}>Comenzar</Button>
+    </main>
   )
 }
