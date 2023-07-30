4934846265d92352e65171a8634338e7280637b6
vladernn <vladys.3234@gmail.com>
2023-07-30 14:28:55 +0200

    add getInitialCards utility

---
+++ src/pages/MemoryGame/utilities/get-initial-cards.js
@@ -0,0 +1,32 @@
+import image3 from '../images/3.png';
+import image4 from '../images/4.png';
+import image5 from '../images/5.png';
+import image8 from '../images/8.png';
+import image9 from '../images/9.png';
+import image10 from '../images/10.png';
+import image15 from '../images/15.png';
+import image16 from '../images/16.png';
+import image17 from '../images/17.png';
+
+export function getInitialCards() {
+    return [
+        { imgSrc: image3, revealed: false, row: 1 },
+        { imgSrc: image4, revealed: false, row: 1 },
+        { imgSrc: image5, revealed: false, row: 1 },
+        { imgSrc: image8, revealed: false, row: 1 },
+        { imgSrc: image9, revealed: false, row: 1 },
+        { imgSrc: image10, revealed: false, row: 1 },
+        { imgSrc: image15, revealed: false, row: 1 },
+        { imgSrc: image16, revealed: false, row: 1 },
+        { imgSrc: image17, revealed: false, row: 1 },
+        { imgSrc: image3, revealed: false, row: 2 },
+        { imgSrc: image4, revealed: false, row: 2 },
+        { imgSrc: image5, revealed: false, row: 2 },
+        { imgSrc: image8, revealed: false, row: 2 },
+        { imgSrc: image9, revealed: false, row: 2 },
+        { imgSrc: image10, revealed: false, row: 2 },
+        { imgSrc: image15, revealed: false, row: 2 },
+        { imgSrc: image16, revealed: false, row: 2 },
+        { imgSrc: image17, revealed: false, row: 2 },
+    ];
+}
