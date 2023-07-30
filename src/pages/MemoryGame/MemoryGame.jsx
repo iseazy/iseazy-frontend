06e3cc91b55daad5f25ccbf6609a9ca863dd328e
vladernn <vladys.3234@gmail.com>
2023-07-30 14:28:34 +0200

    add format time utility

--- src/pages/MemoryGame/MemoryGame.jsx
+++ src/pages/MemoryGame/MemoryGame.jsx
@@ -3,17 +3,9 @@ import './MemoryGame.css';
 import Board from './components/Board/Board';
 import CompletedDialog from './components/CompletedDialog/CompletedDialog';
 import { useEffect } from 'react';
-import image3 from './images/3.png';
-import image4 from './images/4.png';
-import image5 from './images/5.png';
-import image8 from './images/8.png';
-import image9 from './images/9.png';
-import image10 from './images/10.png';
-import image15 from './images/15.png';
-import image16 from './images/16.png';
-import image17 from './images/17.png';
 import useShuffle from './hooks/useShuffle';
 import { getInitialCards } from './utilities/get-initial-cards';
+import { formatTime } from './utilities/format-time';
 
 export default function MemoryGame() {
     const [score, setScore] = useState(0);
@@ -70,12 +62,6 @@ export default function MemoryGame() {
         setTimer(0);
         setIsCompletedDialogOpen(false);
         setInitialCards(getInitialCards());
-    }
-
-    const formatTime = (seconds) => {
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        return `${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
     }
 
     return (
